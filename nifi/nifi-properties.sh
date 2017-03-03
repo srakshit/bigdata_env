e788123c40a172383249fd5a2ea974f18295aa19
Subham Rakshit <developersubham@gmail.com>
2017-03-03 01:05:54 +0000

    Replaced hardcoded path with environment variable

--- nifi/nifi-properties.sh
+++ nifi/nifi-properties.sh
@@ -1,14 +1,14 @@
 !#/bin/sh
 
-sed -i -e "s|^nifi.web.http.host=.*$|nifi.web.http.host=$(hostname)|" /opt/nifi/conf/nifi.properties
-sed -i -e "s|^nifi.cluster.node.address=.*$|nifi.cluster.node.address=$(hostname)|" /opt/nifi/conf/nifi.properties
-sed -i -e "s|^nifi.remote.input.host=.*$|nifi.remote.input.host=$(hostname)|" /opt/nifi/conf/nifi.properties
-sed -i -e "s|^nifi.remote.input.socket.port=.*$|nifi.remote.input.socket.port=8081|" /opt/nifi/conf/nifi.properties
-sed -i -e "s|^nifi.cluster.is.node=.*$|nifi.cluster.is.node=true|" /opt/nifi/conf/nifi.properties
-sed -i -e "s|^nifi.cluster.flow.election.max.candidates=.*$|nifi.cluster.flow.election.max.candidates=3|" /opt/nifi/conf/nifi.properties
-sed -i -e "s|^nifi.cluster.node.protocol.port=.*$|nifi.cluster.node.protocol.port=8082|" /opt/nifi/conf/nifi.properties
-sed -i -e "s|^nifi.zookeeper.connect.string=.*$|nifi.zookeeper.connect.string=zoo-master:2181|" /opt/nifi/conf/nifi.properties
+sed -i -e "s|^nifi.web.http.host=.*$|nifi.web.http.host=$(hostname)|" $NIFI_HOME/conf/nifi.properties
+sed -i -e "s|^nifi.cluster.node.address=.*$|nifi.cluster.node.address=$(hostname)|" $NIFI_HOME/conf/nifi.properties
+sed -i -e "s|^nifi.remote.input.host=.*$|nifi.remote.input.host=$(hostname)|" $NIFI_HOME/conf/nifi.properties
+sed -i -e "s|^nifi.remote.input.socket.port=.*$|nifi.remote.input.socket.port=8081|" $NIFI_HOME/conf/nifi.properties
+sed -i -e "s|^nifi.cluster.is.node=.*$|nifi.cluster.is.node=true|" $NIFI_HOME/conf/nifi.properties
+sed -i -e "s|^nifi.cluster.flow.election.max.candidates=.*$|nifi.cluster.flow.election.max.candidates=3|" $NIFI_HOME/conf/nifi.properties
+sed -i -e "s|^nifi.cluster.node.protocol.port=.*$|nifi.cluster.node.protocol.port=8082|" $NIFI_HOME/conf/nifi.properties
+sed -i -e "s|^nifi.zookeeper.connect.string=.*$|nifi.zookeeper.connect.string=zookeeper:2181|" $NIFI_HOME/conf/nifi.properties
 
-cat /opt/nifi/conf/nifi.properties
+cat $NIFI_HOME/conf/nifi.properties
 
-/opt/nifi/bin/nifi.sh run
+$NIFI_HOME/bin/nifi.sh run
