01d09a8fa7a9505fd06d8ec77db3b8d02446780e
Subham Rakshit <developersubham@gmail.com>
2017-03-04 20:16:38 +0000

    Updated create topic script with full path

--- kafka/kafka-create-topics.sh
+++ kafka/kafka-create-topics.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-total_topic=$(cat kafka-topic-configuration.json | jq '. | length')
+total_topic=$(cat /opt/kafka-topic-configuration.json | jq '. | length')
 
 cnt=0
 
-zookeeper_connect=$(sed -n "/^zookeeper.connect=.*$/p" kafka/config/server.properties | awk -F "=" '{print $2}')
+zookeeper_connect=$(sed -n "/^zookeeper.connect=.*$/p" $KAFKA_HOME/config/server.properties | awk -F "=" '{print $2}')
 
 #Loop through topic-configuration.json and create topics
 while [ "$cnt" -lt "$total_topic" ]
 do
-    topic_name=$(cat kafka-topic-configuration.json | jq '.'[$cnt].name | tr -d '"')
-    topic_partitions=$(cat kafka-topic-configuration.json | jq '.'[$cnt].partitions | tr -d '"')
-    topic_replication_factor=$(cat kafka-topic-configuration.json | jq '.'[$cnt].replication_factor | tr -d '"')
+    topic_name=$(cat /opt/kafka-topic-configuration.json | jq '.'[$cnt].name | tr -d '"')
+    topic_partitions=$(cat /opt/kafka-topic-configuration.json | jq '.'[$cnt].partitions | tr -d '"')
+    topic_replication_factor=$(cat /opt/kafka-topic-configuration.json | jq '.'[$cnt].replication_factor | tr -d '"')
 
     $KAFKA_HOME/bin/kafka-topics.sh --zookeeper $zookeeper_connect --create --topic $topic_name --replication-factor $topic_replication_factor --partitions $topic_partitions
     
