56660b15aaca1bb62142911c2721440dd4ce57d8
Subham Rakshit <developersubham@gmail.com>
2017-03-05 19:05:21 +0000

    Removed hard coding of kafka port from script

--- kafka/check-kafka-started.sh
+++ kafka/check-kafka-started.sh
@@ -16,12 +16,12 @@ while true; do
     for container in $kafka_containers
     do
         #Somehow I couldn't pass env variable in grep. So had to grep env variable name to get the value from the container
-        #if [ -z $kafka_port ]; then
-        #    kafka_port=$(docker exec --user root $container env | grep "KAFKA_PORT" | awk -F "=" '{print $2}')    
-        #fi
+        if [ -z $kafka_port ]; then
+            kafka_port=$(docker exec --user root $container env | grep "KAFKA_PORT" | awk -F "=" '{print $2}')    
+        fi
         
         #This should return port 9092 -- the port where kafka is listening to
-        has_kafka_started=$(docker exec --user root $container netstat -lnt | grep "9092" | awk '{print $4}' | awk -F ":::" '{print $2}')
+        has_kafka_started=$(docker exec --user root $container netstat -lnt | grep "$kafka_port" | awk '{print $4}' | awk -F ":::" '{print $2}')
     
         #If the above command returns value add 1 to the varible total_kafka_started
         if [ ! -z $has_kafka_started ]; then
