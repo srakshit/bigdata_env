1268cfeb7b10bcb61d858aedaf4ddeff8cd86473
Subham Rakshit <developersubham@gmail.com>
2017-03-06 23:32:28 +0000

    WIP: Adding Apache storm

---
+++ storm/nimbus/storm-start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#set storm properties
+#echo "Setting Storm properties"
+#/opt/storm-properties.sh
+
+#start storm
+#echo "Starting Storm"
+#exec $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
