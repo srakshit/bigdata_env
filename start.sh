946be6e8f8487c02125aa29841c8f294c2852fb9
Subham Rakshit <developersubham@gmail.com>
2017-03-09 23:50:20 +0000

    docker-compose yml file updated with zookeeper and ports config

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 docker-compose down
 docker-compose build
 docker-compose up -d
-docker-compose scale nifi=3 kafka=3
+docker-compose scale nifi=3 kafka=3 storm-supervisor=3
 
 ./kafka/check-kafka-started.sh
