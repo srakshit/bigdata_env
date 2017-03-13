e1e6502ee90d7c6efeceb049b565b0c7fdb28393
Subham Rakshit <developersubham@gmail.com>
2017-03-13 22:44:18 +0000

    Removed kafka doanlaod file fromthis proejct to kafka base box project

--- kafka/kafka-download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Downloading and unzipping kafka-$KAFKA_VERSION"
-mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
-url="$mirror/kafka/$KAFKA_VERSION/kafka_$SCALA_VERSION-$KAFKA_VERSION.tgz"
-curl $url | tar xz -C /opt 
