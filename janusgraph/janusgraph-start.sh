0a93cb95b7b3a3026c5755ba73efa57d16d25ab5
Subham Rakshit <developersubham@gmail.com>
2017-05-22 23:42:31 +0100

    Updated janusgraph docker file and exposed cassandra port 9160

--- janusgraph/janusgraph-start.sh
+++ janusgraph/janusgraph-start.sh
@@ -4,4 +4,4 @@
 /opt/janusgraph-properties.sh
 
 #start janusgraph
-$JANUSGRAPH_HOME/bin/janusgraph.sh start
+$JANUSGRAPH_HOME/bin/gremlin-server.sh $JANUSGRAPH_HOME/conf/gremlin-server/rest-gremlin-server.yaml
